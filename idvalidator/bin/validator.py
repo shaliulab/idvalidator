280a58844d4e5da43e25bf14e3dc1d6a4fccd93c
antortjim <ntoniohu@gmail.com>
2021-12-11 10:48:20 +0000

    Update idvalidator

--- idvalidator/bin/validator.py
+++ idvalidator/bin/validator.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import os.path
+import pickle
 
 import numpy as np
 import joblib
@@ -14,7 +15,10 @@ def get_parser(ap=None):
         ap = argparse.ArgumentParser()
 
     ap.add_argument("--experiment-folder", "--input", dest="input", required=True)
-    ap.add_argument("--ncores", type=int, default=-2, required=True)
+    ap.add_argument("--ncores", type=int, default=-2)
+    ap.add_argument("--output", type=str, required=True)
+    ap.add_argument("--number-of-animals", type=int, required=True)
+    ap.add_argument("--body-length-px", type=int, required=True)
     return ap
 
 def main(ap=None, args=None):
@@ -27,14 +31,23 @@ def main(ap=None, args=None):
 
     folders = os.listdir(args.input)
     folders = [folder for folder in folders if is_idtrackerai_folder(os.path.join(args.input, folder))]
+    folders = sorted(folders)
     files = {folder: pick_blob_collection(os.path.join(args.input, folder)) for folder in folders}
+
+
+    #output = check_blobs(list(files.values())[0], number_of_animals=args.number_of_animals, body_length_px=args.body_length_px)
 
     output = joblib.Parallel(n_jobs=args.ncores)(
         joblib.delayed(check_blobs)(
-            blobs_file
+            blobs_file, number_of_animals=args.number_of_animals, body_length_px=args.body_length_px
         ) for blobs_file in files.values()
     )
-    import ipdb; ipdb.set_trace()
+
+
+    output = {folders[i]: output[i] for i in range(len(output))}
+
+    with open(args.output, "wb") as fh:
+        pickle.dump(output, fh)
 
 
 if __name__ == "__main__":
--- idvalidator/validator.py
+++ idvalidator/validator.py
@@ -1,12 +1,15 @@
 import argparse
 import itertools
 from collections import namedtuple
+import logging
 
 import numpy as np
 from scipy.spatial.distance import cdist
 
 from idtrackerai.list_of_blobs import ListOfBlobs
 from trajectorytools.trajectories.concatenate import _compute_distance_matrix
+
+logger = logging.getLogger(__name__)
 
 
 Validation = namedtuple("Validation", ["file", "identified", "tracked", "swaps"])
@@ -22,26 +25,46 @@ def check_blob_has_identity(blob):
 
 
 def check_all_identities_are_found(blobs_in_frame, identities):
-    tracked_identities = list(itertools.chain(*[blob.final_identity for blob in blobs_in_frame]))
+    tracked_identities = list(itertools.chain(*[blob.final_identities for blob in blobs_in_frame]))
+    #tracked_identities = [*i if isinstance(i, list) else i for i in tracked_identities]
     return [i for i in tracked_identities if not i in identities] # empty if all ids are found
 
 
 def get_centroids(blobs_in_frame):
 
-    blobs_in_frame = sorted(blobs_in_frame, key=lambda blob: blob.final_identity)
+    blobs_in_frame = sorted(blobs_in_frame, key=lambda blob: blob.final_identities)
     centroids = np.vstack([blob.centroid for blob in blobs_in_frame])
     return centroids
 
 
-def centroids_swap(centroids_previous, centroids_next, body_length_px):
+def centroids_swap(centroids_previous, centroids_next, body_length_px, number_of_animals):
 
-    number_of_animals = centroids_previous.shape[0]
     jump_size = 1 # units of body size
 
+    n_animals_previous = centroids_previous.shape[0]
+    n_animals_next = centroids_next.shape[0]
+    diff = n_animals_previous - n_animals_next
+    if diff != 0:
+        padding = np.array(
+            [[0., 0.]] * np.abs(diff)
+        )
+        if diff < 0:
+            centroids_previous = np.vstack([
+                centroids_previous,
+                padding
+            ])
+        else:
+            centroids_next = np.vstack([
+                centroids_next,
+                padding
+            ])
+
+    assert centroids_previous.shape[0] == centroids_next.shape[0]
     distances = _compute_distance_matrix(centroids_previous, centroids_next)
+
     swap = np.bitwise_and(
         (distances < body_length_px * jump_size),
-        np.eye(number_of_animals) == 0
+        np.eye(centroids_previous.shape[0]) == 0
     )
 
     id_previous, id_next = np.where(swap)
@@ -52,21 +75,31 @@ def centroids_swap(centroids_previous, centroids_next, body_length_px):
     return swap_ids
 
 
-def blobs_swap(blobs_in_frame_previous, blobs_in_frame_next):
+def blobs_swap(blobs_in_frame_previous, blobs_in_frame_next, **kwargs):
 
     centroids_previous = get_centroids(blobs_in_frame_previous)
     centroids_next = get_centroids(blobs_in_frame_next)
-    swap = centroids_swap(centroids_previous, centroids_next)
+    swap = centroids_swap(centroids_previous, centroids_next, **kwargs)
 
 
-def check_blobs(blob_file, number_of_animals):
+def check_blobs(blob_file, number_of_animals, **kwargs):
     list_of_blobs = ListOfBlobs.load(blob_file)
-    identities = list(range(number_of_animals))
+    identities = list(range(1, number_of_animals+1))
 
     blobs = list_of_blobs.blobs_in_video
 
     frames_fully_identified = [check_blobs_f(blobs_in_frame, check_blob_has_identity) for blobs_in_frame in blobs]
     frames_fully_tracked = [check_all_identities_are_found(blobs_in_frame, identities) for blobs_in_frame in blobs]
-    identities_dont_swap = [blobs_swap(blobs[i], blobs[i+1]) for i in range(len(blobs)-1)]
+    #identities_dont_swap = [blobs_swap(blobs[i], blobs[i+1], number_of_animals=number_of_animals, **kwargs) for i in range(len(blobs)-1)]
+
+    identities_dont_swap = []
+    for i in range(len(blobs)-1):
+        try:
+            data = blobs_swap(blobs[i], blobs[i+1], number_of_animals=number_of_animals, **kwargs)
+        except Exception as error:
+            logger.error(f"Problem with {blob_file} at frame {i}")
+            logger.error(error)
+        else:
+            identities_dont_swap.append(data)
 
     return Validation(blob_file, frames_fully_identified, frames_fully_tracked, identities_dont_swap)
