46e9777d6eaaaa038c64ebb0067dac846430c226
antortjim <ntoniohu@gmail.com>
2021-12-11 20:56:25 +0000

    Update idvalidator

--- idvalidator/bin/id_controller.py
+++ idvalidator/bin/id_controller.py
@@ -15,20 +15,21 @@ from idtrackerai.utils.py_utils import get_spaced_colors_util
 import idvalidator.bin.validator
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 def get_parser(ap=None):
 
     if ap is None:
         ap = argparse.ArgumentParser()
-    
+
     ap.add_argument("--experiment-folder", "--input", dest="input", type=str, required=True)
     ap.add_argument("--chunk", type=int, required=True)
     ap.add_argument("--output", required=True, type=str, help="Output folder with .csv and .mp4 files")
     return ap
 
 def load_data(session_folder):
-    
+
     video_object = np.load(os.path.join(session_folder, "video_object.npy"), allow_pickle=True).item()
     colors = get_spaced_colors_util(
         video_object.user_defined_parameters["number_of_animals"],
@@ -48,7 +49,7 @@ def load_store(experiment_folder, chunk=None):
     lowres_store = imgstore.new_for_filename(os.path.join(experiment_folder, "lowres"), chunk_numbers=chunk)
     return store, lowres_store
 
- 
+
 def compute_step(framerate, ms_res=10):
     step = int((1000/framerate)/ms_res)*ms_res # in units of 10 ms
     return step
@@ -60,25 +61,32 @@ def main(ap=None, args=None):
         args = ap.parse_args()
 
     session_name = f"session_{str(args.chunk).zfill(6)}"
-    session_folder = os.path.join(args.experiment_folder, session_name)
-    experiment_name = os.path.basename(args.experiment_folder.rstrip("/"))
+    session_folder = os.path.join(args.input, session_name)
+    experiment_name = os.path.basename(args.input.rstrip("/"))
 
 
     validation_pickle = os.path.join(args.output, experiment_name + ".pkl")
+    os.makedirs(args.output, exist_ok=True)
     validation_args = Namespace(input=session_folder, output=validation_pickle)
     validation = idvalidator.bin.validator.single_validator(args=validation_args)
-    
+
     missing=[0 if non_id in [None, True] else len(non_id) for non_id in validation[session_name].identified]
-    problem_frames = np.where(np.diff([0] + missing) > 0)
+    problem_frames = np.where(np.diff([0] + missing) > 0)[0]
 
     time_window_length = 5 # seconds
 
-    store, lowres_store = load_store(args.experiment_folder, args.chunk)
-    
+    store, lowres_store = load_store(args.input, args.chunk)
+
     fps = int(round(store._metadata["framerate"]))
 
     # windows = [(frame_number - 1 * fps):(frame_number + (time_window_length - 1)*fps)]
     start_frames = problem_frames - 1 * fps # start one second in the past
+
+    start_frames = np.stack([[0,] * len(start_frames), start_frames], axis=1)
+    start_frames = start_frames.max(axis=1)
+    start_str = ' '.join([str(e) for e in start_frames])
+    logger.info(f"Episodes start -> {start_str}")
+
     step = compute_step(lowres_store._metadata["framerate"], 10)
 
     list_of_blobs, colors = load_data(session_folder)
@@ -94,13 +102,17 @@ def main(ap=None, args=None):
 def make_episode(blobs_in_video, store, chunk, frame_in_chunk, output_folder, time_window_length=5000, step=50, colors=None):
 
     (frame, (framenumber_start, timestamp_start)) = store.get_image(store._index.get_chunk_metadata(chunk)["frame_number"][frame_in_chunk])
-    os.makedirs(output_folder, exist_ok=True)    
+    os.makedirs(output_folder, exist_ok=True)
     timestamps = list(range(int(timestamp_start), int(timestamp_start + time_window_length), step))
-    # check_feed(blobs)
+
+    print("*****")
+    print(timestamps)
+    print("*****")
 
     kwargs={
+        "experiment_folder": os.path.dirname(store.full_path),
         "output_folder": output_folder,
-        "blobs_in_video": blobs_in_video, 
+        "blobs_in_video": blobs_in_video,
         "colors": colors,
         "chunk": chunk
     }
@@ -120,26 +132,29 @@ def make_episode(blobs_in_video, store, chunk, frame_in_chunk, output_folder, ti
 
 def check_feed(blobs_in_video, timestamp, output_folder, colors=None, stores=None, experiment_folder=None, chunk=None):
 
+    assert not (stores is None and experiment_folder is None)
+
     dest = os.path.join(output_folder, f"{str(int(timestamp)).zfill(10)}.png")
 
-    if os.path.exists(dest):
-        return None
+    #if os.path.exists(dest):
+    #    return None
 
     if stores is None:
         if chunk is not None:
             chunk_numbers = [chunk]
+        logger.warning(experiment_folder)
         store = imgstore.new_for_filename(experiment_folder, chunk_numbers=chunk_numbers)
         lowres_store = imgstore.new_for_filename(os.path.join(experiment_folder, "lowres"), chunk_numbers=chunk_numbers)
 
     else:
         store, lowres_store = stores
 
-    chunk_first_frame = store._get_chunk_metadata()["frame_number"][0]
+    chunk_first_frame = store._get_chunk_metadata(chunk)["frame_number"][0]
 
 
     (frame, (fn, _)) = store._get_image_by_time(timestamp)
     (lowres_frame, (fn_lowres, _)) = lowres_store._get_image_by_time(timestamp)
-    
+
     lowres_frame = cv2.resize(lowres_frame, frame.shape[::-1], cv2.INTER_AREA)
 
     frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
@@ -151,12 +166,12 @@ def check_feed(blobs_in_video, timestamp, output_folder, colors=None, stores=Non
         blob.draw(frame, colors_lst=colors, is_selected=False)
 
     frames = [frame, lowres_frame]
-    
+
     fig = []
-    
+
     for i in range(2):
         fig.append(frames[i][:,:,[2,1,0]])
-    
+
     fig = np.hstack(fig)
     cv2.imwrite(dest, fig)
     return fig
